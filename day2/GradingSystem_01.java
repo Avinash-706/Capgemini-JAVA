9835b7c99613d3347bc3c2015984cf3fba8046c9
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-21 12:40:08 +0530

    Grading System 01

---
+++ day2/GradingSystem_01.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class GradingSystem_01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Marks : ");
+        int marks = sc.nextInt();
+        
+        if   (marks >= 90 && marks <= 100)  System.out.print("Grade A");
+        else if(marks >= 75 && marks < 90)  System.out.print("Grade B");
+        else if(marks >= 60 && marks < 75)  System.out.print("Grade C");
+        else if(marks >= 40 && marks < 60)  System.out.print("Grade D");
+        else if(marks <  40 && marks >= 0)  System.out.print("FAIL!!");
+        else                                System.out.print("Invalid Marks!!");
+    }
+}

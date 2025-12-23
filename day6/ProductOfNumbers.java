520cf289f83a0ca7ab77da18521937adc5276f21
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-23 11:32:22 +0530

    Product of All Numbers From 1 to 10

---
+++ day6/ProductOfNumbers.java
@@ -0,0 +1,9 @@
+
+public class ProductOfNumbers {
+    public static void main(String[] args){
+        int res = 1;
+        for( int i = 1 ; i <= 10 ; i++ )     res *= i;
+        System.out.print("Product Of Numbers from 1 to 10 : " + res);
+    }
+    
+}

9518bd443b985b9fadc9d333cf229cd58aab7779
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-12-23 10:13:13 +0530

    Printing Characters in Range -- Do-While Loop

---
+++ day6/PrintCharacters_DoWhile.java
@@ -0,0 +1,22 @@
+public class PrintCharacters_DoWhile {
+    public static void printLoop1(char n1, char n2) {
+        System.out.println("\n\nPrint :\t" + n1 + " to " + n2);
+        do {
+            System.out.print(n1++ + "\t");
+        } while (n1 <= n2);
+    }
+
+    public static void printLoop2(char n1, char n2) {
+        System.out.println("\n\nPrint :\t" + n1 + " to " + n2);
+        do {
+            System.out.print(n1-- + "\t");
+        } 
+        while (n1 >= n2);
+    }
+
+    public static void main(String[] args) {
+
+        printLoop2('s', 'l');
+        printLoop1('T', 'Z');
+    }
+}
